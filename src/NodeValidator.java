28b25823a6264893e5f615bdc6ef85c8a65fd809
Vardhman Mehta <vardhman.mehta96@gamil.com>
2015-07-09 15:03:37 +0530

    added dom parse

---
+++ src/NodeValidator.java
@@ -0,0 +1,5 @@
+/**
+ * Created by VardhmanMehta on 09/07/15.
+ */
+public class NodeValidator {
+}
--- src/WizRocket_Validator.java
+++ src/WizRocket_Validator.java
@@ -1,8 +1,35 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by VardhmanMehta on 09/07/15.
  */
 public class WizRocket_Validator {
     public static void main(String[] args) {
-        System.out.println(args[0]);
+        String filePath = args[0];
+        File file = new File(filePath);
+        System.out.println(filePath);
+        Document dom;
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        String userPackage;
+        try {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            dom = db.parse(file);
+            Element docElm = dom.getDocumentElement();
+            userPackage = docElm.getAttribute("package");
+            NodeList usesPermList = docElm.getElementsByTagName("uses-permission");
+            if (usesPermList == null || usesPermList.getLength() < 1) return;
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
